8af77399a25824fe714f8d613430870a2436b675
robbeca <xmfkddl1006@gmail.com>
2022-10-21 17:57:37 +0900

    Commit Header

---
+++ header/js.js
@@ -0,0 +1,7 @@
+var more = document.querySelector('.nav-more');
+var dropMenu = document.querySelector('.more-drop-menu');
+var navItem = document.querySelector('.nav-item');
+
+more.addEventListener('click', function(e){
+	dropMenu.style.display = 'inline-block';
+});

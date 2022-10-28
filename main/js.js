46110f62cfbac43ffec2f78437121f91a2f44209
robbeca <xmfkddl1006@gmail.com>
2022-10-29 00:54:59 +0900

    Modify timeline

--- main/js.js
+++ main/js.js
@@ -1,4 +1,11 @@
+var menu = document.querySelector('.menu');
+var dropDown = document.querySelector('.drop-down');
+
 function onClickUpload() {
 	const mediaInput = document.querySelector('.media-input');
 	mediaInput.click();
 }
+
+menu.addEventListener('click', function(){
+	dropDown.classList.toggle('show-drop-down');
+});
